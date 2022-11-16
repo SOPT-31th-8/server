a936b29d2ae6577c8b84cadb638fdf4273de8fee
seunghaLim <seungha.lim@kurlycorp.com>
2022-11-16 11:53:23 +0900

    <feat>: liveController 및 라이브 상품 조회 컨트롤러 추가
    liveController 및 liveController 내 라이브 상픔 조회 컨트롤러 기능 추가

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,7 +1,9 @@
 import userController from "./userController";
 import itemController from "./itemController";
+import liveController from "./liveController";
 
 export {
     userController,
-    itemController
+    itemController,
+    liveController 
 };
