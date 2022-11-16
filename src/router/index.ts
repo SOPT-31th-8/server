2e387506a523ae97d628ab0f30f8d59517bf3db8
seunghaLim <seungha.lim@kurlycorp.com>
2022-11-16 11:54:23 +0900

    <feat>: liveRouter 및 /live path 추가
    liveRouter 및 liveRouter 내 라이브 상품 조회 path 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
 import itemRouter from "./itemRouter";
+import liveRouter from "./liveRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
 router.use("/item", itemRouter);
+router.use("/live", liveRouter);
 
 export default router;
