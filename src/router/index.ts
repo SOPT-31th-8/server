7d6628d7592f68df8bb1396ddd6cc6528d93d547
seunghaLim <seungha.lim@kurlycorp.com>
2022-11-16 11:16:30 +0900

    itemRouter 추가 및 item/:itemId path 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
+import itemRouter from "./itemRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
+router.use("/item", itemRouter);
 
 export default router;
