cf7452374ba3dd8ee190cf9d13b909a0fa224ea4
Yewon So <soyw96@gmail.com>
2022-11-16 19:50:58 +0900

    <feat>: bestRouter 및 item/best path 추가
    bestRouter 및 bestRouter 내 베스트 상품 리스트 조회 path 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
 import itemRouter from "./itemRouter";
+import bestRouter from "./bestRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
 router.use("/item", itemRouter);
+router.use("/item", bestRouter);
 
 export default router;
