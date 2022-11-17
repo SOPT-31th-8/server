52ec4fe95be7aaa71de3020f556ec9cc327630c3
Yewon So <soyw96@gmail.com>
2022-11-17 14:27:30 +0900

    <feat>: customRouter 및 /custom path 추가
    customRouter 및 customRouter 내 맞춤 상품 조회 path 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
 import itemRouter from "./itemRouter";
+import customRouter from "./customRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
 router.use("/item", itemRouter);
+router.use("/custom", customRouter);
 
 export default router;
