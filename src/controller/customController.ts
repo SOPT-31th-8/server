f48b0a4bf4a98c1ea63ac36f77f58fa5f1e8514d
Yewon So <soyw96@gmail.com>
2022-11-17 13:55:24 +0900

    <feat>: customController 추가 및 맞춤 상품 리스트 조회 컨트롤러 추가
    customController 및 customController 내 맞춤 상품 리스트 조회뷰 조회 컨트롤러 기능 추가

---
+++ src/controller/customController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { customService } from "../service";
+
+const getCustoms = async (req: Request, res: Response) => {
+
+    const data = await customService.getCustomItem();
+
+    if(!data) {
+        return res.status(500).json({ "status": 500, "success": false, "message": "INTERNAL SERVER ERROR" }) 
+    }
+
+    return res.status(200).json({ "status":200, "success": true, "message": "맞춤 상품 리스트 조회뷰 조회 성공"})
+};
+
+const customController = {
+    getCustoms,
+};
+
+export default customController;
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,7 +1,9 @@
 import userController from "./userController";
 import itemController from "./itemController";
+import customController from "./customController";
 
 export {
     userController,
-    itemController
+    itemController,
+    customController
 };
