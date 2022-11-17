5f7d38a10170a2ac6251c15f50a07ac800a7d0d9
Yewon So <soyw96@gmail.com>
2022-11-17 20:31:39 +0900

    <hotfix3>

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,11 +1,9 @@
-import userController from "./userController";
 import itemController from "./itemController";
 import bestController from "./bestController";
 import liveController from "./liveController";
 import customController from "./customController";
 
 export {
-    userController,
     itemController,
     bestController,
     liveController,
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import userRouter from "./userRouter";
 import itemRouter from "./itemRouter";
 import liveRouter from "./liveRouter";
 import bestItemRouter from "./bestItemRouter";
@@ -8,7 +7,6 @@ import customRouter from "./customRouter";
 
 const router: Router = Router();
 
-router.use("/user", userRouter);
 router.use("/item", itemRouter);
 router.use("/live", liveRouter);
 router.use("/best-item", bestItemRouter);
